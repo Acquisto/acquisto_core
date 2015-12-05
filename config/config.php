56d1ad1f14c593963dc13f1ba113b2443d71c876
Sascha.Brandhoff <sascha.brandhoff@gmail.com>
2015-12-05 13:26:05 +0100

    Added manufacture DCA for Backend

--- config/config.php
+++ config/config.php
@@ -2,12 +2,27 @@
 
 $GLOBALS['BE_MOD']['acquisto'] = array
 (
+	'acquisto_manufacture' => array
+	(
+		'tables'     => array('tl_shop_manufacture'),
+		'icon'       => 'system/modules/acquisto_core/assets/icons/be/cog.png',
+	),
 	'acquisto_category' => array
 	(
 		'tables'     => array('tl_shop_category'),
 		'icon'       => 'system/modules/acquisto_core/assets/icons/be/application_cascade.png',
 	)
 );
+
+$GLOBALS['BE_MOD']['acquisto_settings'] = array
+(
+	'acquisto_tax' => array
+	(
+		'tables'     => array('tl_shop_tax', 'tl_shop_taxrate'),
+		'icon'       => 'system/modules/acquisto_core/assets/icons/be/ruby.png',
+	)
+);
+
 
 array_insert($GLOBALS['FE_MOD']['acquisto'], 0, array
 (
